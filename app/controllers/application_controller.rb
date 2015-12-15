4ff1ba317eea3f0dbe2647789de18f8b7e458cdb
Andrew Ellison <aellison1558@gmail.com>
2015-12-15 10:20:44 -0800

    verify sign up/sign in functioning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   end
 
   def sign_out
-    @current_user.reset_token!
+    current_user.reset_token!
     session[:session_token] = nil
     @current_user = nil
   end
